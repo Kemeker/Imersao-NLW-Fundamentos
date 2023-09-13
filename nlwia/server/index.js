2e11e5a47299d754764c25fe9f7fda61e546be7c
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-09-13 15:17:42 +0000

    add att

--- nlwia/server/index.js
+++ nlwia/server/index.js
@@ -4,4 +4,8 @@ import express from 'express'
 const app = express()
 app.use(cors())
 
+app.get('/summary/:id', (request, response) => {
+    response.send('ID do video: '+ request.params.id)
+})
+
 app.listen(3333, () => console.log('server escutando na porta 3333'))
