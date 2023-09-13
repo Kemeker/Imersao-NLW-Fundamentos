e7db719562fa25c5f7d9eb84acca8b7e4f326928
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-09-13 15:35:56 +0000

    add biblioteca de ytdl

--- nlwia/server/download.js
+++ nlwia/server/download.js
@@ -2,6 +2,9 @@ import ytdl from 'ytdl-core'
 import fs from  'fs'
 
 export function download(videoId) {
+    const videoURL = "https://www.youtube.com.br/shorts/" + videoId
     console.log('Fazendo Download do video: '+ videoId)
-
+    
 }
+
+ytdl(videoURL, {quality: "lowestaudio", filter: "audioonly"})
