9a47120ced7230e69bc20a8eca97d4b38791c82d
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-09-14 12:12:50 -0300

    add forms

--- nlwia/web/form.js
+++ nlwia/web/form.js
@@ -1,1 +1,9 @@
 const form = document.querySelector("#form")
+const input = document.querySelector("#input")
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault()
+
+    const videoURL = input.value
+    console.log("URL do video: ", videoURL)
+})
